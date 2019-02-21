ae707adfc3c1aab1cf09e7849ae3532bedc4e895
Denis Novikov <voothi@gmail.com>
2019-02-21 10:49:18 +0300

    fix student page

--- controllers/TeacherController.php
+++ controllers/TeacherController.php
@@ -8,7 +8,7 @@ use yii\web\Controller;
 
 class TeacherController extends Controller
 {
-    public function actionStudentNew()
+    public function actionStudent()
     {
         $comment_view = "Test Yii2 Framework param controller view";
         return $this->render('student', ['test_controller_view' => $comment_view]);
